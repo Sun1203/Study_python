a011616759782b4eebfea5a775578bb3dd4840cb
khk <jasun1208@naver.com>
2021-06-13 16:14:39 +0900

    문제

--- 연습문제.py
+++ 연습문제.py
@@ -11,3 +11,53 @@ t = 110                     # 다운로드 하는데 걸리는 시간
 result = (r * t) / 1000     # 속도 * 시간 나누기 1000
 print(result)               # 88MB
 
+
+
+'''
+
+Q1
+다음 코드의 결괏값은 무엇일까?
+
+a = "Life is too short, you need python"
+
+if "wife" in a: print("wife")
+elif "python" in a and "you" not in a: print("python")
+elif "shirt" not in a: print("shirt")
+elif "need" in a: print("need")
+else: print("none")
+
+'''
+
+# 결과값 : shirt
+
+'''
+Q2
+ while문을 사용해 1부터 1000까지의 자연수 중
+ 3의 배수의 합을 구해 보자.
+'''
+
+a = 0
+b = 1
+while b < 1000:
+    if a % 3 == 0:
+        a += b
+    b = b + 1
+print(a)
+
+'''
+Q3
+while문을 사용하여 다음과 같이 별(*)을
+ 표시하는 프로그램을 작성해 보자.
+
+*
+**
+***
+****
+*****
+'''
+
+i = 0
+while True:
+    i += 1              # while문 수행 시 1씩 증가
+    if i > 5: break     # i 값이 5보다 크면 while문을 벗어난다.
+    print ('*' * i)     # i 값 개수만큼 *를 출력한다
